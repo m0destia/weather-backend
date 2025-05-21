3780c9e265f2fcebb9616c3f937b3da1641cf745
Vinícius <viniciussz1470@outlook.com>
2025-05-21 19:34:02 -0300

    ignored.

---
+++ script.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import axios from 'axios';
+import cors from 'cors';
+import 'dotenv/config';
+
+const app = express();
+app.use(cors({
+    origin: ''
+}));
+
+const PORT = process.env.PORT || 3000;
+const API_KEY = process.env.API_KEY;
+
+app.get('/clima', async (req, res) => {
+    const city = req.query.city || 'London';
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric&lang=pt_br`;
+
+    try {
+        const response = await axios.get(url);
+        res.json(response.data);
+
+    } catch (error) {
+        res.status(500).json({
+            error: 'Erro ao buscar dados do clima. Verifique a cidade ou tente novamente mais tarde.'
+        });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando no localhost:${PORT}`);
+});
